a9a062adb470cb1b007f46715ae5f29594e3ab91
C K <ctkoo@ucsc.edu>
2022-07-01 11:21:09 -0700

    functional local storage (lost sleep over this)

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,33 @@
+console.log($(".saveBtn"));
+
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+
+    var desc = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id").split("-")[1];
+    
+
+    localStorage.setItem(hour, desc);
+
+  });
+
+// Accessing local storage
+$("#hour-08 .description").val(localStorage.getItem("08"));
+$("#hour-09 .description").val(localStorage.getItem("09"));
+$("#hour-10 .description").val(localStorage.getItem("10"));
+$("#hour-11 .description").val(localStorage.getItem("11"));
+$("#hour-12 .description").val(localStorage.getItem("12"));
+$("#hour-13 .description").val(localStorage.getItem("13"));
+$("#hour-14 .description").val(localStorage.getItem("14"));
+$("#hour-15 .description").val(localStorage.getItem("15"));
+$("#hour-16 .description").val(localStorage.getItem("16"));
+$("#hour-17 .description").val(localStorage.getItem("17"));
+$("#hour-18 .description").val(localStorage.getItem("18"));
+$("#hour-19 .description").val(localStorage.getItem("19"));
+
 // For setting up the current date
 var current = moment();
-$("#currentDay").text(current.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+$("#currentDay").text(current.format("dddd, MMMM Do YYYY"));
 
 var currentHr = current.format("HH"); // Take the current hour in 24-hour format, will be useful later
 
